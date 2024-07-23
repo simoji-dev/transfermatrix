c9ba02ae07e4b051ff081f9ae2496e22425fa24c
chaenisch <christian.haenisch@posteo.de>
2024-07-24 00:28:57 +0200

    feat: init Layer with Callable for reading nk_file

--- src/transfermatrix/input.py
+++ src/transfermatrix/input.py
@@ -1,17 +1,31 @@
 import pydantic
 from enum import Enum
+from pathlib import Path
+from typing import Callable, Optional
 
 
-class Layer(pydantic.BaseModel):
+class OpticalConstants(pydantic.BaseModel):
 
-    wavelengths: list[float]        # wavelengths in nm
-    n: list[float]                  # refractive index (real part)
-    k: list[float]                  # extinction coefficient
+    wavelengths: list[float]    # wavelengths in nm
+    n: list[float]              # refractive index (real part)
+    k: list[float]              # extinction coefficient
 
 
-class CoherentLayer(Layer):
+class Layer:
 
-    thickness: float                # layer thickness in nm
+    def __init__(self, nk_file: Path, nk_reader: Callable[[Path], OpticalConstants], thickness: Optional[float] = None,
+                 is_coherent: Optional[bool] = False):
+
+        optical_constants: OpticalConstants = nk_reader(nk_file)
+
+        self.wavelengths = optical_constants.wavelengths
+        self.n = optical_constants.n
+        self.k = optical_constants.k
+
+        self.thickness: float | None = thickness
+        self.is_coherent: bool = is_coherent
+
+        assert (isinstance(self.thickness, float) or ((self.thickness is None) and (not self.is_coherent)))
 
 
 class Polarization(str, Enum):
