433ee015a19d6017e27395a6deb896b654b5da26
chaenisch <christian.haenisch@posteo.de>
2025-03-06 22:12:13 +0100

    refactor: define optical constants and layer as dataclass

--- notebooks/test.py
+++ notebooks/test.py
@@ -1,9 +1,10 @@
-from transfermatrix.input import Layer
+from transfermatrix.input import Layer, OpticalConstants
 
 wl = [100, 200, 300]
 n = [1.1, 1.2, 1.3]
 k = [0, 0, 0]
 
-layer = Layer(wavelengths=wl, n=n, k=k)
+optical_constants = OpticalConstants(wavelengths=wl, n=n, k=k)
+layer = Layer(optical_constants, thickness=10.)
 
 print(layer)
--- src/transfermatrix/input.py
+++ src/transfermatrix/input.py
@@ -1,31 +1,29 @@
+import dataclasses
 import pydantic
 from enum import Enum
 from pathlib import Path
 from typing import Callable, Optional
 
 
-class OpticalConstants(pydantic.BaseModel):
+@dataclasses.dataclass
+class OpticalConstants:
+    """Wavelengths-dependent optical constants of a material."""
 
     wavelengths: list[float]    # wavelengths in nm
     n: list[float]              # refractive index (real part)
     k: list[float]              # extinction coefficient
 
 
+@dataclasses.dataclass
 class Layer:
+    """Two-dimensional layer of an optical layer stack."""
 
-    def __init__(self, nk_file: Path, nk_reader: Callable[[Path], OpticalConstants], thickness: Optional[float] = None,
-                 is_coherent: Optional[bool] = False):
+    optical_constants: OpticalConstants
+    thickness: float | int | None
+    is_coherent: bool = False
 
-        optical_constants: OpticalConstants = nk_reader(nk_file)
-
-        self.wavelengths = optical_constants.wavelengths
-        self.n = optical_constants.n
-        self.k = optical_constants.k
-
-        self.thickness: float | None = thickness
-        self.is_coherent: bool = is_coherent
-
-        assert (isinstance(self.thickness, float) or ((self.thickness is None) and (not self.is_coherent)))
+    def __post_init__(self):
+        assert (isinstance(self.thickness, float | int) or ((self.thickness is None) and (not self.is_coherent)))
 
 
 class Polarization(str, Enum):
